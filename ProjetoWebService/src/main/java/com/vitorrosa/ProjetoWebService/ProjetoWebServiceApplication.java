26c7b1748a638c250301d0143ffb931fa54c5930
vitorrosa0 <vrfrosa1@gmail.com>
2025-04-17 11:06:00 -0300

    Project created

---
+++ ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/ProjetoWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vitorrosa.ProjetoWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServiceApplication.class, args);
+	}
+
+}
---
+++ ProjetoWebService/src/test/java/com/vitorrosa/ProjetoWebService/ProjetoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorrosa.ProjetoWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
