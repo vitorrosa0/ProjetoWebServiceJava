f02d8dc8a1f00fc8fe7336503d0a68263c145666
vitorrosa0 <vrfrosa1@gmail.com>
2025-04-21 22:22:41 -0300

    Service layer, component registration

--- ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/resources/UserResource.java
+++ ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.vitorrosa.ProjetoWebService.resources;
 
 import com.vitorrosa.ProjetoWebService.entities.User;
+import com.vitorrosa.ProjetoWebService.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+    public ResponseEntity<List<User>> findAll(){
+        List<User> u = userService.findAll();
         return ResponseEntity.ok().body(u);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
