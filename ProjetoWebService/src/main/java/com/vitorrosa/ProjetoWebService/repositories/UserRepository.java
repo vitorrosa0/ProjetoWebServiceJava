179210c7b7219d462dae2a5229fb16bef5b94be6
vitorrosa0 <vrfrosa1@gmail.com>
2025-04-21 21:51:30 -0300

    Revert "JPA repository, dependency injection, database seeding"
    This reverts commit 8cf10cbfb361fe244c29924fc3f3a8785610d2e6.

--- ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vitorrosa.ProjetoWebService.repositories;
-
-import com.vitorrosa.ProjetoWebService.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
