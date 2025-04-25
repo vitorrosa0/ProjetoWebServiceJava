9844e1d3a099588f2f51293c4bef805a50cc55b9
vitorrosa0 <vrfrosa1@gmail.com>
2025-04-25 16:23:00 -0300

    User insert

--- ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/resources/UserResource.java
+++ ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.vitorrosa.ProjetoWebService.entities.User;
 import com.vitorrosa.ProjetoWebService.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,9 +22,17 @@ public class UserResource {
         List<User> u = userService.findAll();
         return ResponseEntity.ok().body(u);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
