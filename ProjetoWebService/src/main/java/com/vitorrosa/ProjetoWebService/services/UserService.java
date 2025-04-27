13b04ae5c22612b4deb20868443771e93521d674
vitorrosa0 <vrfrosa1@gmail.com>
2025-04-27 15:57:39 -0300

    Exception handling - findById

--- ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/services/UserService.java
+++ ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/services/UserService.java
@@ -2,6 +2,7 @@ package com.vitorrosa.ProjetoWebService.services;
 
 import com.vitorrosa.ProjetoWebService.entities.User;
 import com.vitorrosa.ProjetoWebService.repositories.UserRepository;
+import com.vitorrosa.ProjetoWebService.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
