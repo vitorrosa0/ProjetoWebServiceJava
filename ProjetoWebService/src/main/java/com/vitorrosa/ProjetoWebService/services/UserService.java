0db0363c1f675a0e62e9073f341c68b2c5c1d196
vitorrosa0 <vrfrosa1@gmail.com>
2025-04-28 11:20:14 -0300

    Exception handling - delete

--- ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/services/UserService.java
+++ ProjetoWebService/src/main/java/com/vitorrosa/ProjetoWebService/services/UserService.java
@@ -2,8 +2,11 @@ package com.vitorrosa.ProjetoWebService.services;
 
 import com.vitorrosa.ProjetoWebService.entities.User;
 import com.vitorrosa.ProjetoWebService.repositories.UserRepository;
+import com.vitorrosa.ProjetoWebService.services.exceptions.DatabaseException;
 import com.vitorrosa.ProjetoWebService.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
